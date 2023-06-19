a561dc287507522a7ddc856cbf9db0e6e87f3cd3
Ridwain <115967876+Ridwain@users.noreply.github.com>
2023-06-19 20:12:20 +0600

    Add files via upload

---
+++ str.py
@@ -0,0 +1,9 @@
+names=['Ridwain','Siat','Torsa']
+names.append('Tasnim')
+print(names)
+print(names[1])
+del names[2]
+print(names)
+new=names.pop(0)
+print(new)
+print(names)
---
+++ test.py
@@ -0,0 +1,12 @@
+#print("Hello World")
+#print("First Program- Python print function")
+#print('It is declared like this :')
+#print("print('What to print')")
+#input("What's your name ?\n")
+#print("Hello"+" "+input("What Is Your Name="))
+
+#print("Hey"+" "+input("What is your name?\n")+","+"How Are You?")
+print("String Manipulation Exercise")
+print("String Concatenation is Done With "+" Sign")
+print('e.g. print("Hello"+"Jenny")')
+print("New lines will be created with a backslash and n")
